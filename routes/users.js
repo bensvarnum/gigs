96145139f506d50f23e170e3aedb08c957a8b43e
bensvarnum <bens.varnum@gmail.com>
2021-04-04 20:37:22 -0400

    setup passport

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+const User = require("../models").User
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -7,6 +9,45 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" })
+  }
+
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" })
+  }
+  if (password.length < 6) {
+    errors.push({ msg: "Passwords should be at least 6 characters" })
+  }
+
+  if (errors.length > 0) {
+    res.render('register', { errors, name, email, password, password2 })
+  } else {
+    const user = await User.findOne({ where: { email: email } })
+    if (user) {
+      errors.push({ msg: "Email is already registered" })
+      res.render('register', { errors, name, email, password, password2 })
+    }
+    if (!user) {
+      const hashedPassword = await bcrypt.hash(password, 10)
+      const newUser = await User.create({
+        name,
+        password: hashedPassword,
+        email
+      })
+      req.flash(
+        'success_msg',
+        'You are now registered and can log in'
+      );
+      res.redirect('/users/login');
+    }
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const db = require("./models");
 
 const app = express();
 
-const db = require("./models");
+require('./config/passport-local')(passport);
+
 db.sequelize.sync();
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(
+    session({
+        secret: 'shhhhhhhhhhhh',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
